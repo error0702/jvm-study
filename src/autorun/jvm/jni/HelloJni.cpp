68b0243ab82ff426b400b06e526dae8cc3f3de16
Autorun <20293349@qq.com>
2021-11-12 01:43:34 +0800

    update jni c++

--- src/autorun/jvm/jni/HelloJni.cpp
+++ src/autorun/jvm/jni/HelloJni.cpp
@@ -1,10 +1,11 @@
 //
 // Created by autorun on 2021/11/12.
 //
-
+#include <iostream>
 #include "HelloJni.h"
+using namespace std;
 
-JNIEXPORT void JNICALL Java_HelloJni_hello(JNIEnv *, jobject)
+JNIEXPORT void JNICALL Java_HelloJni_hello(JNIEnv *env, jobject obj)
 {
-    jobject
+    cout << "hello JNI!" << endl;
 }
